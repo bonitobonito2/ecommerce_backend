24ad30c0fc3e1dc874b9b1ef81fc3ea65a9c417b
bonitobonito2 <zaal.tsagareli.1btu.edu.ge>
2023-02-21 20:06:19 +0400

    ecrypt password

--- src/auth/test.entity.ts
+++ src/auth/test.entity.ts
@@ -8,6 +8,8 @@ import {
   PrimaryGeneratedColumn,
   OneToMany,
 } from 'typeorm';
+import { MessageEncryptionConfig } from './messageEncryptionConfig/messageEncryptionConfig';
+import { EncryptionTransformer } from 'typeorm-encrypted';
 
 @Entity('user')
 export class Users {
@@ -26,6 +28,14 @@ export class Users {
   @Column()
   gender: string;
 
-  @Column()
+  @Column('text', {
+    name: 'password',
+    transformer: new EncryptionTransformer({
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56',
+    }),
+  })
   password: string;
 }

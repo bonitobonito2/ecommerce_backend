6f8d9621696cde3ad4f74ca91a2d45d3f1d4ac2d
bonitobonito2 <zaal.tsagareli.1btu.edu.ge>
2023-02-21 20:06:05 +0400

    some logic to save information

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './test.entity';
+import { registrationDto } from './dtos/registration.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,23 +10,22 @@ export class AuthService {
   private readonly messagesRepository: Repository<Users>;
 
   public async getEverything() {
-    const data = await this.messagesRepository.query('select * from user');
+    const data = await this.messagesRepository.find();
     console.log(data);
     return data;
   }
-  public async save() {
-    const data = await this.messagesRepository.insert({
-      first_name: 'dito',
-      last_name: 'lastname',
-      gender: 'male',
-      email: 'dito@gmail.com',
+  public async registar(data: registrationDto) {
+    const checkIfEmailExsists = await this.messagesRepository.findOneBy({
+      email: data.email,
     });
-    return data;
-  }
-
-  public async delete() {
-    const data = await this.messagesRepository.query('DELETE FROM user ');
-
-    return 'deleted every single raw';
+    if (checkIfEmailExsists) return 'email already exsists';
+    const registar = await this.messagesRepository.insert({
+      first_name: data.first_name,
+      last_name: data.last_name,
+      gender: data.gender,
+      email: data.email,
+      password: data.password,
+    });
+    return registar;
   }
 }

39bff48236acb962c4f1380638774b776ed0aa1a
bonitobonito2 <zaal.tsagareli.1btu.edu.ge>
2023-02-27 15:44:19 +0400

    registared passport jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,24 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Users } from './test.entity';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}

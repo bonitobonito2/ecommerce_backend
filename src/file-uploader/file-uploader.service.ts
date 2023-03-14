dae70de4a179302bd53da64c5fffd7cf19b46c02
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-14 12:52:37 +0400

    removed unused logic

--- src/file-uploader/file-uploader.service.ts
+++ src/file-uploader/file-uploader.service.ts
@@ -3,22 +3,16 @@ import { Users } from 'src/auth/entities/user.entity';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Repository } from 'typeorm';
-import { ProfilePictures } from './entities/profileImages.entity';
+import { UserPosts } from './entities/posts.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class FileUploaderService {
-  @InjectRepository(ProfilePictures)
-  private readonly profilePictureRepo: Repository<ProfilePictures>;
+  @InjectRepository(UserPosts)
+  private readonly profilePictureRepo: Repository<UserPosts>;
 
   public async uploadProfilePicture(file, header) {
     const user = header['user'];
-
-    await this.profilePictureRepo.delete({ user_id: user.id });
-    await this.profilePictureRepo.insert({
-      user_id: user.id,
-      picture_code: user.id,
-    });
     return fs.writeFileSync(`./src/uploads/${user.id}.png`, file.buffer);
   }
 
